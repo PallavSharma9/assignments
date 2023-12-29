7080a3086f4ca03cec877afb14bbefadeddf05ac
Pallav Sharma <pallavsharma0202@gmail.com>
2023-12-29 22:27:39 +0530

    answer

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,18 @@
 */
 
 function isAnagram(str1, str2) {
+  let string1 = str1.split('').sort()
+  let string2 = str2.split('').sort()
+  console.log(string1)
+  console.log(string2)
 
+  if(string1.toString() === string2.toString()){
+    return true;
+  }
+  else return false;
 }
+console.log(isAnagram('spar','rasp'))
+console.log(isAnagram('spar','ssar'))
+
 
 module.exports = isAnagram;
